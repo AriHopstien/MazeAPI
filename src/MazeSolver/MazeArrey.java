b4059d8638cb8c54fe41f012168fcd0c6bbaf054
ari hop <arihopstien@gmail.com>
2026-06-09 23:31:05 +0300

    chg

--- src/MazeSolver/MazeArrey.java
+++ src/MazeSolver/MazeArrey.java
@@ -1,31 +1,32 @@
 package MazeSolver;
+import MazeConstraction.GetInstructions;
 
 public class MazeArrey {
-    public static int[][] solveMaze(int[][] maze) {
-        int rows = maze.length;
-        int cols = maze[0].length;
+    public static int[][] solveMaze(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
 
         int[][] result = new int[rows][cols];
 
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
 
-                if (maze[row][col] == 0) {
+                if (grid[row][col] == 0) {
                     result[row][col] = 0;
                     continue;
                 }
                 int count = 0;
 
-                if (row > 0 && maze[row - 1][col] == 1) {
+                if (row > 0 && grid[row - 1][col] == 1) {
                     count++;
                 }
-                if (row < rows - 1 && maze[row + 1][col] == 1) {
+                if (row < rows - 1 && grid[row + 1][col] == 1) {
                     count++;
                 }
-                if (col > 0 && maze[row][col - 1] == 1) {
+                if (col > 0 && grid[row][col - 1] == 1) {
                     count++;
                 }
-                if (col < cols - 1 && maze[row][col + 1] == 1) {
+                if (col < cols - 1 && grid[row][col + 1] == 1) {
                     count++;
                 }
                 result[row][col] = count;
