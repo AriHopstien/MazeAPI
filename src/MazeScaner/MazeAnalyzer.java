99c3d1f1b64097867e68ad6df023061fd1bb1b9c
ari hop <arihopstien@gmail.com>
2026-06-09 18:36:04 +0300

    chg

--- src/MazeScaner/MazeAnalyzer.java
+++ src/MazeScaner/MazeAnalyzer.java
@@ -1,4 +1,8 @@
 // שלב 7
+
+//צריך להחזיר מערך דו מימי ברוחב width ובגובה height
+// המערך יכיל מספרים בין 0 ל5 כמבואר בשורה הבאה
+// 0=Wall 1=DeadEnd 2=Way 3-4=Node 5=End
 package MazeScaner;
 
 public class MazeAnalyzer {
